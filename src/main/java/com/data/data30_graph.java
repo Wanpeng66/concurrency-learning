00c1a4f854bbb1dd83e32f6191cfb55826905057
wanpeng <18717792412@163.com>
2020-04-08 22:28:57 +0800

    无向图的简单实现

--- src/main/java/com/data/data30_graph.java
+++ src/main/java/com/data/data30_graph.java
@@ -1,5 +1,9 @@
 package com.data;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author: wp
  * @Title: data30_graph
@@ -9,4 +13,24 @@ package com.data;
 public class data30_graph {
 
 
+}
+//无向图
+class MyGraph{
+    int v;
+    List<List<Integer>> list = null;
+
+    MyGraph(int v){
+        this.v = v;
+        list = new ArrayList<>( v+1 );
+        for(int i=0;i<v+1;i++){
+            list.add( new LinkedList<Integer>(  ) );
+        }
+    }
+
+    public void addNode(int base,int target){
+        List<Integer> mine = list.get( base );
+        mine.add( target );
+        List<Integer> other = list.get( target );
+        other.add( base );
+    }
 }
